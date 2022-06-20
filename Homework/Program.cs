19ac5003997a0c80d77d1a769c3c2a1bc3c9864c
saidsn <saidsn@code.edu.az>
2022-06-21 02:35:52 +0400

    C#code

---
+++ Homework/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. DivideNums(21);
+            //2. SumNumber(6, 4);
+            //3. OddNumbers(0, 10);
+            //4. int[] nums = { 2, 7, 9, 11, 8, 10 };
+            //   ArrOddNmberSum(nums);
+            //5. int[] numbers = { 2, 7, 9, 11, 8, 10 };
+            //   ArrEvenNumberSum(numbers);
+            //6. PrimeCompose(10);
+        }
+
+
+        //1.n is the division of the number into 3 and 7.
+
+        //static void DivideNums(int n)
+        //{
+        //    if (n % 3 == 0 && n % 7 == 0)
+        //    {
+        //        Console.WriteLine("divide");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("not divide");
+        //    }
+        //}
+
+
+        //2.If n and m are both pairs, add n and m.
+
+        //static void SumNumber(int n, int m)
+
+        //{
+        //    if (n % 2 == 0 && m % 2 == 0)
+        //    {
+        //        Console.WriteLine(n + m);
+        //    }
+
+        //}
+
+        // 3.Find the sum of the single numbers between the given n and m(n<m) numbers.
+
+        //static void OddNumbers(int n, int m)
+        //{
+        //    int sum = 0;
+        //    for (int i = n; i < m; i++)
+        //    {
+        //        if (n < m && i % 2 == 1)
+        //        {
+        //            sum += i;
+        //        }
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+        // 4.Find the sum of the single numbers in the given array.
+        //static void ArrOddNmberSum(int[] nums)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i] % 2 == 1)
+        //        {
+        //            sum += nums[i];
+        //        }
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+        // 5.Find the number of even numbers in the given array.
+
+        //static void ArrEvenNumberSum(int[] numbers)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        if (numbers[i] % 2 == 0)
+        //        {
+        //            count ++;
+        //        }
+        //    }
+        //    Console.WriteLine(count); 
+        //}
+
+        // 6.Find out if the given item is simple or complex.
+
+        //static void PrimeCompose(int n)
+        //{
+        //    int count = 0;
+        //    if (n <= 1)
+        //    {
+        //        Console.WriteLine(false);
+        //    }
+        //    else
+        //    {
+        //        for (int i = 1; i <= n; i++)
+        //        {
+        //            if (n % i == 0)
+        //            {
+        //                count++;
+        //            }
+        //            else
+        //            {
+        //                if (count > 2)
+        //                {
+        //                    Console.WriteLine("composile");
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine("prime");
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+            
+
+
+
+
+
+
+
+    }
+}
